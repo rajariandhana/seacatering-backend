d4139861bbfb4ee141c0413980a5821d5da79524
Ralfazza Rajariandhana <rajariandhana@gmail.com>
2025-06-28 13:51:59 +1000

    access middleware

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 
+import { Request, Response } from 'express';
+
 import authController from '../controllers/auth.controller';
 import dummyController from '../controllers/dummy.controller';
 import subscriptionController from '../controllers/subscription.controller';
 import testimonialController from '../controllers/testimonial.controller';
 import planController from '../controllers/plan.controller';
 
+import { ROLES } from '../utils/constant';
 import authMiddleware from '../middleware/auth.middleware';
+import aclMiddleware from '../middleware/acl.middleware';
 
 const router = express.Router();
 
@@ -16,7 +20,8 @@ router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 router.get('/auth/me', authMiddleware, authController.me);
 
-router.post('/subscription/create', subscriptionController.create);
+// router.post('/subscription/create', subscriptionController.create);
+router.post('/subscription/create', [authMiddleware, aclMiddleware([ROLES.MEMBER])], subscriptionController.create)
 
 router.get('/testimonial/find-all', testimonialController.findAll);
 router.post('/testimonial/create', testimonialController.create);
